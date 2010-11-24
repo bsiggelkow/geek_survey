2418727212ab344fc7b7e3d55d38a2eb2a5dc838
Bill Siggelkow <bsiggelkow@mac.com>
2010-11-24 16:28:09 -0500

    birth

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GeekSurvey::Application.config.secret_token = '984823e81258e1affe5f8ec18d61a8ea1ec09ff6f48e54dfcf96c224c9533a327b0c138d177e925dec59b9d9947c7dd5088931931e1db64715b850caad7a0197'
